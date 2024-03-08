ded43deb9b7d32500ad6f06d0c55932ac09b2a2d
Ameya <ameya.nagnur18@gmail.com>
2024-03-09 01:41:20 +0530

    pick id / jwt from authorization header

--- Middlewares/Auth.ts
+++ Middlewares/Auth.ts
@@ -2,10 +2,34 @@ import { Middleware } from "../Helpers/types";
 import { UserModel } from "../Models/UserResult";
 import { createUser, getUserDetails } from "../Services/Auth";
 import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
+import { statusCode } from "Helpers/response";
 
+/* 
+    Guest Login with just an id to maintain a partial 
+    session since location data is not very sensitive 
+    (alternatively we have a login option in UserRouter)
+*/
 export const validateOrGuestLogin: Middleware = async (request, response, next) => {
-    const user: UserModel = request.params.id ? 
-        await getUserDetails(request.params.id) : null;
+    
+    // Id / Jwt passed as token after 'BearerToken {id/jwt}' in auth header.
+    const authHeader = request.headers.authorization;
+    const token = authHeader ? authHeader.split(' ')[1] : null;
+
+    // If we implement the login functionality using UserRouter:
+    /*
+    jwt.verify(token, process.env.JWT_SECRET, function(error, decoded: jwt.JwtPayload) {
+        if (error) {
+            return response.status(statusCode.unAuthorized).send({ message: "Authorization Failed", error });
+        }
+
+        response.locals.user = {
+            id: decoded.nameid
+        }
+    });
+    */
+
+    const user: UserModel = token ? await getUserDetails(token) : null;
 
     var userId;
     if (!user) {
